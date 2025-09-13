6f3ff200e5e24763cd13e5dc68bbdd0601a92060
clane83 <clane83@yahoo.com>
2025-09-13 13:04:49 -0700

    checkin create, update and delete user update to destination create.  To also use current date time in yyyy-mm-dd format

---
+++ routes/bucket-list.js
@@ -0,0 +1,3 @@
+const express = require('express').Router();
+
+const users = require('../controllers/users')
--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
