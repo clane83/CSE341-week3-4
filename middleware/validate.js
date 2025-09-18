4809a5d1b33da75d3d0802c9bf0004664c1b066f
clane83 <clane83@yahoo.com>
2025-09-18 16:27:23 -0700

    checkin update for savedestination error handling.  similar to saveUser validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -27,26 +27,32 @@ const saveUser = (req, res, next) => {
 
 
 const saveDestination = (req, res, next) => {
-    const validationRule = {
-        country: 'required|string',   // fixed key name
-        cities_to_visit: 'required|array',
-        languages: 'required|array',
-        createdby: 'required|string',
-        // createdon: 'required|date',  //set by the server
-        visited: 'required|string',
+    //Only validate for POST/PUT
+    if (req.method === 'POST' || req.method === 'PUT') {
+        const validationRule = {
+            country: 'required|string',   // fixed key name
+            cities_to_visit: 'required|array',
+            languages: 'required|array',
+            createdby: 'required|string',
+            // createdon: 'required|date',  //set by the server
+            visited: 'required|string',
 
-    };
-    validate(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-            return res.status(400).json({
-                success: false,
-                message: 'Validation failed',
-                data: err
-            });
-        } else {
-            return next();
-        }
-    });
+        };
+        validate(req.body, validationRule, {}, (err, status) => {
+            if (!status) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'Validation failed',
+                    data: err
+                });
+            } else {
+                return next();
+            }
+        });
+    } else {
+        return next();
+    }
+
 };
 
 module.exports = { saveUser, saveDestination };
