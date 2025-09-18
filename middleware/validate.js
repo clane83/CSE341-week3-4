656c8441b0a4e6f1760b4241957f54715b039581
clane83 <clane83@yahoo.com>
2025-09-18 16:19:57 -0700

    checkin validation update to handle POST/PUT request to have required fields and DELETE/GET to skip the required fields

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,21 +1,28 @@
 const validate = require('../helpers/validate');
 
 const saveUser = (req, res, next) => {
-    const validationRule = {
-        username: 'required|string',   // fixed key name
-        password: 'required|string'
-    };
-    validate(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-            return res.status(400).json({
-                success: false,
-                message: 'Validation failed',
-                data: err
-            });
-        } else {
-            return next();
-        }
-    });
+    //Only validate for POST/PUT
+    if (req.method === 'POST' || req.method === 'PUT') {
+        const validationRule = {
+            username: 'required|string',   // fixed key name
+            password: 'required|string'
+        };
+        validate(req.body, validationRule, {}, (err, status) => {
+            if (!status) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'Validation failed',
+                    data: err
+                });
+            } else {
+                return next();
+            }
+        });
+    } else {
+        //skips validation for DELETE/GET
+        return next()
+    }
+
 };
 
 
