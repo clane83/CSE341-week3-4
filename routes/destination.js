2a5990691d7acc4364eb10517b61606b108443b5
clane83 <clane83@yahoo.com>
2025-09-20 08:32:10 -0700

    checkin readding validation to user and destination routes

--- routes/destination.js
+++ routes/destination.js
@@ -2,14 +2,23 @@ const express = require('express')
 const router = express.Router();
 
 const destinationController = require('../controller/destination')
-// const validation = require('../middleware/validate')
+const validation = require('../middleware/validate')
 const { isAuthenticated } = require("../middleware/authentication")
 
 router.get('/', destinationController.allDestinations);
 
-router.post('/', isAuthenticated, destinationController.createDestination);
-router.put('/:id', isAuthenticated, destinationController.updateDestination);
-router.delete('/:id', isAuthenticated, destinationController.deleteDestination);
+router.post('/',
+    isAuthenticated,
+    validation.saveDestination,
+    destinationController.createDestination);
+router.put('/:id',
+    isAuthenticated,
+    validation.saveDestination,
+    destinationController.updateDestination);
+router.delete('/:id',
+    isAuthenticated,
+    validation.saveDestination,
+    destinationController.deleteDestination);
 
 module.exports = router;
 
--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,24 @@ const express = require('express')
 const router = express.Router();
 
 const usersController = require('../controller/users')
-// const validation = require('../middleware/validate')
+const validation = require('../middleware/validate')
 const { isAuthenticated } = require("../middleware/authentication")
 
 router.get('/', usersController.allUsers);
 
 
-router.post('/', isAuthenticated, usersController.createUser);
-router.put('/:id', isAuthenticated, usersController.updateUser);
-router.delete('/:id', isAuthenticated, usersController.deleteUser);
+router.post('/',
+    isAuthenticated,
+    validation.saveUser,
+    usersController.createUser);
+router.put('/:id',
+    isAuthenticated,
+    validation.saveUser,
+    usersController.updateUser);
+router.delete('/:id',
+    isAuthenticated,
+    validation.saveUser,
+    usersController.deleteUser);
 
 module.exports = router;
 
