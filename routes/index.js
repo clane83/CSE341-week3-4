d27d10d1652c14fb9ff7a5a7ca97317469f99186
clane83 <clane83@yahoo.com>
2025-09-19 21:56:24 -0700

    checkin logout error handling

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,25 @@ router.use('/', require('./swagger'));
 
 router.get('/login', passport.authenticate('github'), (req, res) => { });
 
-router.get('/logout', function (req, res, next) {
-    req.logout(function (err) {
-        if (err) { return next(err); }
+router.get('/logout', (req, res, next) => {
+    req.logout(err => {
+        if (err) return next(err);
+
+        req.session.destroy(err2 => {
+            if (err2) return next(err2);
+
+            // If you use special cookie flags in prod, mirror them here:
+            // res.clearCookie('connect.sid', { sameSite: 'none', secure: true });
+            res.clearCookie('connect.sid');
+
+            // Swagger prefers JSON; browsers like a redirect.
+            const wantsJSON = (req.get('accept') || '').includes('application/json');
+            if (wantsJSON) return res.status(200).json({ message: 'Logged out' });
+            return res.redirect('/');
+        });
     });
 });
+
 
 
 module.exports = router;
